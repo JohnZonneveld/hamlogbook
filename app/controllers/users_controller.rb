036e6201eaf673347756ad87c68cc762dee5d1b8
raspimeteo <jjczon@gmail.com>
2020-06-04 19:24:18 -0500

    Edited pages and routes changing/deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,37 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index"
-  end
+  	# GET: /users
+  	get "/users" do
+    	erb :"/users/index"
+  	end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new"
-  end
+  	# POST: /users
+  	post "/users" do
+    	redirect "/users"
+  	end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
-  end
+	# GET: /users/5/edit
+  	get "/users/:id/edit" do
+    	erb :"/users/edit"
+  	end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show"
-  end
+  	# GET: /users/5
+  	get "/users/:id" do
+    	erb :"/users/show"
+  	end
 
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit"
-  end
+  	# PATCH: /users/5
+  	patch "/users/:id" do
+      	user=User.find(params[:id])
+		user.update(params[:user])
+    	redirect "/users/:id"
+  	end
 
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
+  	# DELETE: /users/5/delete
+	delete "/users/:id/delete" do
+		user = User.find(params[:id])
+		user.destroy
+		redirect "/logout"
+	end
 
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
 end
